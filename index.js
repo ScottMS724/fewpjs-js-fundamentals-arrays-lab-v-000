4c331e558ba15e2b770410d5d08bfa491ef7da4c
Scott Silverman <scottms724@gmail.com>
2019-12-10 18:24:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const prepend = ["Milo", "Otis", "Garfield"]
 const removeLast = ["Milo", "Otis", "Garfield"]
 const removeFirst = ["Milo", "Otis", "Garfield"]
 
-const append.push("Odie")
+append.push("Odie");
 
-const prepend[0] = "Odie"
+prepend[0] = "Odie";
+
+removeLast.pop();
+
